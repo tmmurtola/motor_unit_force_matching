b64c3cc9fa787425b9552b6efb01ed2078a5b954
tmmurtola <64543076+tmmurtola@users.noreply.github.com>
2024-08-19 14:05:15 +0100

    Added of feedback control and updated MU pool
    Control using combined feedforward-feedback added. Optimisation of feedback gain added. MU pool object updated to allow for step-wise evolution of activation state. Calculation of discrete time correction updated.

--- MUpool.m
+++ MUpool.m
@@ -10,6 +10,11 @@ classdef MUpool
         dt
         name
         opts
+        a1
+        a2
+        a3
+        time_since_fired
+        last_IIT
     end
 
     methods
@@ -25,11 +30,30 @@ classdef MUpool
             obj.tot_number = n;
             obj.dt = dt;
             obj.opts = opts;
+            obj.a1 = zeros(n,1);
+            obj.a2 = zeros(n,1);
+            obj.a3 = zeros(n,1);
+            obj.time_since_fired = inf(n,1);
+            obj.last_IIT = inf(n,1);
+        end
+
+        function obj = reset(obj)
+            %RESET(obj) Reset state variables in MU pool
+            %   Input:
+            %       obj         MUpool object (with n MUs)
+            %   Output:
+            %       obj         MUpool object (with n MUs)
+            obj.a1 = zeros(obj.tot_number,1);
+            obj.a2 = zeros(obj.tot_number,1);
+            obj.a3 = zeros(obj.tot_number,1);
+            obj.time_since_fired = inf(obj.tot_number,1);
+            obj.last_IIT = inf(obj.tot_number,1);
         end
 
         function activation = drive2activation(obj,drive)
             % DRIVE2ACTIVATION(obj, drive) Compute normalised activation 
-            % level of MUs in pool corresponding to neural drive signal
+            % level of MUs in pool corresponding to neural drive signal.
+            % Ignores intial state of MUs.
             %   Input:
             %       obj         MUpool object (with n MUs)
             %       drive       neural drive signal as a row vector (1 x k)
@@ -41,6 +65,26 @@ classdef MUpool
             frs = drive2rate(obj,drive);
             us = rate2excitation(obj,frs);
             activation = excitation2activation(obj,us);
+
+        end
+
+        function [activation,obj] = drive2activationStep(obj,drive)
+            % DRIVE2ACTIVATIONSTEP(obj, drive) Compute normalised activation 
+            % level of MUs in pool corresponding to neural drive signal,
+            % starting from current state of MUs and returning updated MU
+            % state
+            %   Input:
+            %       obj         MUpool object (with n MUs)
+            %       drive       neural drive signal as a row vector (1 x k)
+            %   Output:
+            %       activation  matrix of MU activation signals (n x k)
+            %                   (row i = activation for MU i, col j = time
+            %                   step j)
+            %       obj         MUpool object (with n MUs)
+
+            frs = drive2rate(obj,drive);
+            [us,obj] = rate2excitationStep(obj,frs);
+            [activation,obj] = excitation2activationStep(obj,us);
 
         end
 
@@ -99,32 +143,81 @@ classdef MUpool
             % compute firing times one MU at a time
             for i = 1:obj.tot_number
 
-                % find time window when firing rate > 0 
+                % find time window when firing rate >= min_rate 
                 start_ind = find(frs(i,:)>0,1,'first');
                 end_ind = find(frs(i,:)>0,1,'last');
                 if end_ind == start_ind % frs never goes back to zero
                     end_ind = length(frs(i,:));
                 end
 
-                % desired inter-impulse time
+                % desired inter-impulse times
                 IITs = 1./frs(i,:);
 
-                % first spike when frs>0 for the first time
+                % first spike when frs>=min_rate for the first time
                 excitations(i,start_ind) = 1;
                 time_since_fired = obj.dt;
                 
                 % loop through time steps of interest
                 for j = (start_ind+1):end_ind
                     if IITs(j)<= time_since_fired  % fire at current time step
-                        excitations(i,j) = 1;  
-                        time_since_fired = time_since_fired - IITs(j) + obj.dt; % add discrete time correction
-                    else    % no fire, just update time_since_fired
-                        time_since_fired = time_since_fired + obj.dt;
-                    end
+                        excitations(i,j) = 1;
+                        if isinf(IITs(j-1))    % first & derecruited
+                            time_since_fired = 0;
+                        else
+                            time_since_fired = obj.dt - (IITs(j-1)-time_since_fired+obj.dt)/(1-(IITs(j)-IITs(j-1))/obj.dt); % add discrete time correction
+                        end
+                    end    
+                    % update time_since_fired
+                    time_since_fired = time_since_fired + obj.dt;                    
                 end
             end
 
         end
+
+        function [excitations, obj] = rate2excitationStep(obj,frs)
+            % RATE2EXCITATIONSTEP(obj, frs) Compute excitation impulse trains 
+            % for MUs in pool corresponding to firing rate signals,             
+            % starting from current state of MUs and returning updated MU
+            % state
+            %   Input:
+            %       obj         MUpool object (with n MUs)
+            %       frs         firing rate matrix (n x k)
+            %                   (row i = firing rate for MU i, col j = time
+            %                   step j)
+            %   Output:
+            %       excitations  matrix of excitation signals (n x k)
+            %                   (row i = activation for MU i, col j = time
+            %                   step j)
+            %       obj         MUpool object (with n MUs)
+
+            excitations = zeros(size(frs));
+            
+            % compute firing times one MU at a time
+            for i = 1:obj.tot_number
+
+                % desired inter-impulse times
+                IITs = 1./frs(i,:);
+                
+                % loop through time steps
+                for j = 1:size(frs,2)
+                    if ~isinf(IITs(j))
+                        if IITs(j)<= obj.time_since_fired(i)  % fire at current time step
+                            excitations(i,j) = 1;
+                            if isinf(obj.last_IIT(i))    % first & derecruited
+                                obj.time_since_fired(i) = 0;
+                            else
+                                obj.time_since_fired(i) = obj.dt - (obj.last_IIT(i)-obj.time_since_fired(i)+obj.dt)/(1-(IITs(j)-obj.last_IIT(i))/obj.dt); % add discrete time correction
+                            end
+                        end
+                    end
+                    % update time_since_fired & last IIT
+                    obj.time_since_fired(i) = obj.time_since_fired(i) + obj.dt;
+                    obj.last_IIT(i) = IITs(j);
+                end
+            end
+
+        end
+
 
         function activation = excitation2activation(obj,u)
             % EXCITATION2ACTIVATION(obj, u) Compute normalised activation 
@@ -151,6 +244,33 @@ classdef MUpool
                 a2 = a2 + obj.dt./obj.opts.alpha(:,2).*(a1    -(obj.opts.beta(:,2)+(1-obj.opts.beta(:,2)).*a1)    .*a2);
                 a3 = a3 + obj.dt./obj.opts.alpha(:,3).*(a2    -(obj.opts.beta(:,3)+(1-obj.opts.beta(:,3)).*a2)    .*a3);
                 activation(:,i) = a3;
+            end
+            activation = activation./obj.opts.a_max;
+        end
+
+        function [activation,obj] = excitation2activationStep(obj,u)
+            % EXCITATION2ACTIVATION(obj, u) Compute normalised activation 
+            % level of MUs in pool corresponding to excitation 
+            % impulse trains starting from current state of MUs and 
+            % returning updated MU state
+            %   Input:
+            %       obj         MUpool object (with n MUs)
+            %       u           excitation signal matrix (n x k)
+            %                   (row i = excitation for MU i, col j = time
+            %                   step j)
+            %   Output:
+            %       activation  matrix of MU activation signals (n x k)
+            %                   (row i = activation for MU i, col j = time
+            %                   step j)
+            %       obj         MUpool object (with n MUs)
+
+            activation = zeros(size(u));
+
+            for i = 1:size(u,2)
+                obj.a1 = obj.a1 + obj.dt./obj.opts.alpha(:,1).*(u(:,i)  -(obj.opts.beta(:,1)+(1-obj.opts.beta(:,1)).*u(:,i)).*obj.a1);
+                obj.a2 = obj.a2 + obj.dt./obj.opts.alpha(:,2).*(obj.a1  -(obj.opts.beta(:,2)+(1-obj.opts.beta(:,2)).*obj.a1).*obj.a2);
+                obj.a3 = obj.a3 + obj.dt./obj.opts.alpha(:,3).*(obj.a2  -(obj.opts.beta(:,3)+(1-obj.opts.beta(:,3)).*obj.a2).*obj.a3);
+                activation(:,i) = obj.a3;
             end
             activation = activation./obj.opts.a_max;
         end
